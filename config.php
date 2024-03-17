1e2f8e697576fd09e79fb393f7231c458cbd69fb
khanali454 <khanandkhan061@gmail.com>
2024-03-17 22:59:50 +0500

    new

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 $db_host='localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'nutrio';
-// $db_user = 'u454623698_mansab_ali';
-// $db_pass = 'U454623698_mansab_ali';
-// $db_name = 'u454623698_mansab_ali';
+// $db_user = 'root';
+// $db_pass = '';
+// $db_name = 'nutrio';
+$db_user = 'u454623698_mansab_ali';
+$db_pass = 'U454623698_mansab_ali';
+$db_name = 'u454623698_mansab_ali';
 $conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
