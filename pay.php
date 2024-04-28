f975fd22be01fab317cdacca31bac6ba9775f0d9
khanali454 <khanandkhan061@gmail.com>
2024-04-28 10:13:14 +0500

    moyasar pay

--- pay.php
+++ pay.php
@@ -15,7 +15,7 @@
 <script>
   Moyasar.init({
     element: '.mysr-form',
-    amount: 10,
+    amount: 100,
     currency: 'USD',
     description: 'Coffee Order #1',
     publishable_api_key: 'pk_test_jwZrUJrCuTcKDQ2pLBdwz6UaMuYdCwFo5B9cncc3',
